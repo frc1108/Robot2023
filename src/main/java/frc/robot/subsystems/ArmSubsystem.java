6b2ff01cc16427eff4f07ca7b1f3877847d28e67
Nelson Green <nelson.green@gmail.com>
2023-02-04 11:06:19 -0600

    Added stub subsystems to work on

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+  /** Creates a new ArmSubsystem. */
+// TODO NEO Motor with 20:1 gearbox with current limiting
+// TODO Limit switch or magnetic stops (ideal would be absolute encoder off rotation of arm)
+
+
+  public ArmSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  
+
+//   
+}
